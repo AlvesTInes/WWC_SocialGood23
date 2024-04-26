326ffbee45247c931675d5cec3e5d8f4858f72e0
AlvesTInes <146088070+AlvesTInes@users.noreply.github.com>
2024-04-26 15:56:26 +0100

    Update 03_Question_1.py

--- pages/03_Question_1.py
+++ pages/03_Question_1.py
@@ -117,7 +117,7 @@ col1, col2, col3 = st.columns([2,1,2])
 with col2:
     if st.button('Next'):
         if not st.session_state.Question1:
-            with col1:
+            with col3:
                 st.warning ("Please select an option")
         else:
             switch_page('Question 2')
