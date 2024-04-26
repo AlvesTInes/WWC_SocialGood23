e174e088d9aee05ff65bb96c338be85371c54b08
AlvesTInes <146088070+AlvesTInes@users.noreply.github.com>
2024-04-26 17:37:17 +0100

    Update 03_Question_1.py

--- pages/03_Question_1.py
+++ pages/03_Question_1.py
@@ -115,11 +115,12 @@ st.markdown('***')
 # and chances to the next page ('Question 2')
 col1, col2, col3 = st.columns([2,1,2])
 
-modal = Modal("Please select an option", key="demo-modal")
+modal = Modal("Warning", key="demo-modal")
 with col2:
     open_modal = st.button("Next")
     if open_modal:
-        modal.open()
+        with modal.container():
+            st.markdown("Please select an option")
 
 # Read the value of the items in Session State
 # st.write(st.session_state)
