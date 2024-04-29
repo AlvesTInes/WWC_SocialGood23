47782bd893177c16364e85f7d10a5939b8bcaf95
AlvesTInes <146088070+AlvesTInes@users.noreply.github.com>
2024-04-29 16:33:05 +0100

    Update 02_Let's_begin!.py

--- pages/02_Let's_begin!.py
+++ pages/02_Let's_begin!.py
@@ -77,11 +77,11 @@ space(lines=1)
 # Displaying a select widget, where the user should choose their continent and country
 data = 'https://raw.githubusercontent.com/AlvesTInes/WWC_SocialGood23/main/Countries%20by%20continents.csv'
 df = pd.read_csv(data)
-continent_data = df['continent']
+continent_data = df['Continent']
 continent = st.selectbox('**Please select a continent:**', continent_data, index=None, placeholder="Select a continent", key='select_boxcontinent',
                          on_change=update_continent, disabled=st.session_state.quiz)
 space(lines=1)
-df1 = df.loc[df.continent == continent]
+df1 = df.loc[df.Continent == continent]
 df2 = df1.country
 country = st.selectbox('**Please select a country:**', df2, index=None, placeholder="Select a country", key='select_boxcountry',
                          on_change=update_country, disabled=st.session_state.quiz)
