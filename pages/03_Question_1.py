2ea08851850172351f64dbbdcea8430e06b6a080
AlvesTInes <146088070+AlvesTInes@users.noreply.github.com>
2024-04-30 18:02:59 +0100

    Update 03_Question_1.py

--- pages/03_Question_1.py
+++ pages/03_Question_1.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.let_it_rain import rain
 from streamlit_space import space
 from streamlit_modal import Modal
+from streamlit_gsheets import GSheetsConnection
 from st_pages import add_page_title
 from PIL import Image
 import base64
