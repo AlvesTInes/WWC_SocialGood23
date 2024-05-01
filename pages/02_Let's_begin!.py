fd650420c269f6c76c2cb3321139640085a74db1
AlvesTInes <146088070+AlvesTInes@users.noreply.github.com>
2024-05-01 18:37:49 +0100

    Update 02_Let's_begin!.py

--- pages/02_Let's_begin!.py
+++ pages/02_Let's_begin!.py
@@ -56,7 +56,8 @@ conn= st.connection("gsheets", type=GSheetsConnection)
 
 # Fetch existing data
 existing_data= conn.read(worksheet="FoodWasteWizards",usecols=list(range(5)), ttl=5)
-#existing_data=existing_data.dropna(how='all')
+existing_data=existing_data.dropna(how='all')
+st.dataframe(existing_data)
 
 # Initializing a session state variable called 'quiz' to False; pass the former to the st.button's 'quiz' parameter
 if "quiz" not in st.session_state:
@@ -139,19 +140,19 @@ col1, col2, col3 = st.columns([2,1,2])
 
 with col2:
     if st.button("Let's go!", on_click=disable, disabled=st.session_state.quiz):
-        user_data = pd.DataFrame(
-            [
-                {
-                    "user_id": user_id,
-                    "age": age,
-                    "continent": continent,
-                    "country": country,
-                    "gender_id": gender_id,
-                }
-            ]
-        )
-        updated_df = pd.concat([existing_data, user_data], ignore_index=True)
-        conn.update(worksheet="FoodWasteWizards", data=updated_df)
+        #user_data = pd.DataFrame(
+            #[
+                #{
+                    #"user_id": user_id,
+                    #"age": age,
+                    #"continent": continent,
+                    #"country": country,
+                    #"gender_id": gender_id,
+                #}
+            #]
+        #)
+        #updated_df = pd.concat([existing_data, user_data], ignore_index=True)
+        #conn.update(worksheet="FoodWasteWizards", data=updated_df)
         switch_page('Question 1')
 
 # Read the value of the items in Session State
